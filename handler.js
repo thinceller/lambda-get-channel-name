8e0faa91409743fff29207732bc453938013fff2
thinceller <kohei.ct@icloud.com>
2019-02-18 09:12:26 +0900

    add main script

---
+++ handler.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const request = require('request');
+const cheerio = require('cheerio');
+
+module.exports.index = async (event, context, callback) => {
+  try {
+    const channelName = await scrapeChannelName(event.id);
+    const result = {
+      statusCode: 200,
+      body: { channelName }
+    };
+    callback(null, result);
+  } catch (e) {
+    return e;
+  }
+};
+
+const scrapeChannelName = id => {
+  const url = `https://www.youtube.com/channel/${id}`;
+  return new Promise((resolve, reject) => {
+    request(url, (e, res, body) => {
+      // When any error occurred
+      if (e) {
+        console.log('request error: ', e);
+        reject({
+          statusCode: 500,
+          message: e
+        });
+      }
+
+      // When a channel is not found
+      if (res && res.statusCode === 404) {
+        console.log(`404 not found \nchannel id: ${id}`);
+        reject({
+          statusCode: 404,
+          message: 'not found'
+        });
+      }
+
+      try {
+        const $ = cheerio.load(body);
+        const channelTitle = $('meta[name="title"]').attr('content');
+        resolve(channelTitle);
+      } catch (e) {
+        console.log(e);
+        reject({
+          statusCode: 501,
+          message: e
+        });
+      }
+    })
+  });
+};
